7c4b95c5a0c312fece78bbcfc0498348c5769cd7
Katrina M <merca3@inbox.lv>
2021-04-22 18:38:45 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -19,12 +19,26 @@ $(function() {
             const message = $('#message-input').val();
 
             const d = new Date();
+
+            let day = d.getDate();
+            let month = d.getMonth() + 1;
+            let hour = d.getHours();
+            let minutes = d.getMinutes();
+
+            function checkZero(date) {
+                if (date < 10) {
+                    return `0${date}`;
+                } else {
+                    return date;
+                };
+            };
+
             const newTextBox = `
     <li>
         <div class="card bg-secondary text-light">
             <div class="card-header fs-5 text-info">
                 <span id="name" class="me-5">${username}</span>
-                    <span id="date">${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}</span>
+                    <span id="date">${checkZero(day)}.${checkZero(month)}.${d.getFullYear()} ${checkZero(hour)}:${checkZero(minutes)}</span>
                 </div>
             <div class="card-body m-0 fs-7">
                 <div class="">
